6c6ec9a404b55171cfdf576874e18b9fff5815d1
karreed <jonathanau.au21@gmail.com>
2024-05-18 21:33:41 -0700

    random

--- group-project.js
+++ group-project.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $("#question").keypress(function(event) {
-        if (event.key === 'Enter') {
-            $("#ask").click();
-        }
-    });
+$("#question").keypress(function(event) {
+    if (event.key === 'Enter') {
+        $("#ask").click();
+    }
+});
 
-    $("#ask").click(function() {
-        var question = $("#question").val();
-        alert(question);
-    });
+$("#ask").click(function() {
+    var question = $("#question").val();
+    var yesOrNo = Math.random() < 0.5 ? 'Yes' : 'No';
+    $("#output").html('<div class="text"><p>' + question + "<br />" + yesOrNo + '</p></div>');
+    $("#question").val('');
 });
