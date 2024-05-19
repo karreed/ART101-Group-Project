b755fdc8383ad4f0d2cc15d542cd9c444981352d
karreed <jonathanau.au21@gmail.com>
2024-05-18 22:59:47 -0700

    pages

--- js/group-project.js
+++ js/group-project.js
@@ -1,12 +1,22 @@
+$(document).keydown(function(event) {
+    if (event.key ==='Enter') {
+        if ($("#intro").is(':visible')) {
+            $("#enter").click();
+            event.preventDefault();
+        }
+    }
+});
 
 $("#enter").click(function() {
     $("#intro").hide();
     $("#prompt").show();
+    $("#question").focus();
 });
 
 $("#question").keypress(function(event) {
     if (event.key === 'Enter') {
         $("#ask").click();
+        event.preventDefault();
     }
 });
 
@@ -16,6 +26,14 @@ $("#ask").click(function() {
     $("#prompt").hide();
     $("#main-page").show();
     $("#main-page").append('<div class="text"><p>' + question + '</p></div>');
+});
+
+$(document).keydown(function(event) {
+    if (event.key === 'Backspace' || event.key === 'Escape') {
+        if ($("#main-page").is(':visible')) {
+            $("#back").click();
+        }
+    }
 });
 
 $("#back").click(function() {
