59ee151dfdf53d47795196ffbaaa0ce6c5c3d3a9
karreed <jonathanau.au21@gmail.com>
2024-05-27 16:37:21 -0700

    daisy

--- js/group-project.js
+++ js/group-project.js
@@ -1,3 +1,7 @@
+var daisies = ["daisies/daisy1.png", "daisies/daisy2.png", "daisies/daisy3.png", "daisies/daisy4.png", "daisies/daisy5.png", "daisies/daisy6.png", "daisies/daisy7.png", "daisies/daisy8.png", "daisies/daisy9.png", "daisies/daisy10.png", "daisies/daisy11.png", "daisies/daisy12.png", "daisies/daisy13.png", "daisies/daisy14.png", "daisies/daisy15.png", "daisies/daisy16.png", "daisies/daisy17.png", "daisies/daisy18.png", "daisies/daisy19.png"];
+var i = 0;
+var yesNo = Math.random() < 0.5;
+
 $(document).keydown(function(event) {
     if (event.key ==='Enter') {
         if ($("#intro").is(':visible')) {
@@ -22,10 +26,10 @@ $("#question").keypress(function(event) {
 
 $("#ask").click(function() {
     var question = $("#question").val();
-    $("#question").val('');
     $("#prompt").hide();
     $("#main-page").show();
     $("#main-page").append('<div class="text"><p>' + question + '</p></div>');
+    $("#main-page").append('<div class="yesOrNo">' + (yesNo ? "Yes" : "No") + '</div>');
 });
 
 $(document).keydown(function(event) {
@@ -35,6 +39,16 @@ $(document).keydown(function(event) {
         }
     }
 });
+
+$("#daisy").click(function() {
+    i = (i + 1) % daisies.length
+    if (i === 0) {
+        $("#daisy").hide();
+    }
+    $(this).attr('src', daisies[i]);
+    yesNo = !yesNo;
+    $(".yesOrNo").html((yesNo ? "Yes" : "No"));
+})
 
 $("#back").click(function() {
     $(".text").remove();
